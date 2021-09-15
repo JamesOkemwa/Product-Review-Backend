470316cbc5f2a80a6c10c76e8cda6c18b13dc6ec
JamesOkemwa <okejames55@gmail.com>
2021-09-15 16:38:58 +0300

    Admin Site Customization

--- product_review/reviews/admin.py
+++ product_review/reviews/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Product, Category, Company, ProductSize, ProductSite, Comment
+
+#admin.register() decorator
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'content', )
+    list_filter = ('category', )
 
 # Register your models here.
+admin.site.register((Category, Company, ProductSize, ProductSite, Comment, ))
+
+admin.site.unregister(Group)
+
+admin.site.site_header = "Product Review Admin"
